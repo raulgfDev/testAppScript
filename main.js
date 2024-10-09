a172259aec6a8facf0ebe50369b9d8be6280a733
raul <raulgf1982@gmail.com>
2024-10-09 23:19:58 +0200

    cmm1

---
+++ main.js
@@ -0,0 +1,23 @@
+
+function unaFuncion () {
+  console.log(`Hello ${ mundo }`);
+}
+
+// arrow function, no exe like library ( biblioteca )
+const dosFuncion = () => {
+  console.log('esto es la funcion dos');
+}
+
+/**
+ *
+ * @param { number } dividendo sera el dividendo
+ * @param { number } divisor sera el divisor
+ *
+ * @return { number } sera el resultado del la division
+ * 
+ * @customfunctions
+ * 
+ */
+function division ( dividendo, divisor ) {
+  return dividendo / divisor;
+}
